c1c4996ddad53a05e0b802ce853f63a76ff78ab1
SaieshSharma <saiesh.techscribe@gmail.com>
2024-11-28 23:31:20 +0530

    !!!

--- frontend/src/table/Tables/Columns/FeedbackReleasedColumn.jsx
+++ frontend/src/table/Tables/Columns/FeedbackReleasedColumn.jsx
@@ -45,7 +45,7 @@ export const columnDef = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Start Date
+          Active Until
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
@@ -59,23 +59,5 @@ export const columnDef = [
       return "N/A";
     },
     enableSorting: true,
-  },
-  {
-    accessorKey: "feedbackForm",
-    header: "View Feedback",
-    cell: ({ row }) => (
-      <Button
-        onClick={() => window.open(row.getValue("URL"), "_blank")}
-        className="view-btn"
-      >
-        View <ExternalLink className="ml-2 h-4 w-4" />
-      </Button>
-    ),
-    enableSorting: false,
-  },
-  {
-    accessorKey: "actions",
-    header: "Actions",
-    enableSorting: false,
   },
 ];
